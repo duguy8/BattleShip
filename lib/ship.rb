4ce83df3360669fdfeecbe0ce44e37562767a6cb
Kris Litman <duguy8@gmail.com>
2020-12-07 23:36:25 -0500

    Ship_tests finished & passing. Dbl check syntax

--- lib/ship.rb
+++ lib/ship.rb
@@ -7,4 +7,16 @@ class Ship
     @length = length
     @health = 3
   end
+
+  def hit
+    @health -= 1
+  end
+
+  def sunk?
+    if @health == 0
+      true
+    else
+    false
+    end
+  end
 end
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -1,21 +1,39 @@
  require 'minitest/autorun'
  require 'minitest/pride'
- require '../lib/ship'
+ require './lib/ship'
 
  class ShipTest < Minitest::Test
- def test_it_exists
-   cruiser = Ship.new("Cruiser", 3)
-   assert_instance_of Ship, cruiser
- end
+   def test_it_exists
+     cruiser = Ship.new("Cruiser", 3)
+     assert_instance_of Ship, cruiser
+  end
 
- def test_ship_has_health
- # skip
-   cruiser = Ship.new("Cruiser", 3)
-   assert_equal 3, cruiser.health
- end
+  def test_ship_has_health
+    # skip
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal 3, cruiser.health
+  end
 
- def test_for_more_attributes
-   skip
- assert_equal _, _
- end
+  def test_has_not_sunk
+    # skip
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal false, cruiser.sunk?
+  end
+
+  def test_ship_loses_health_after_hit
+    # skip
+    cruiser = Ship.new("Cruiser", 3)
+    cruiser.hit
+    assert_equal false, cruiser.sunk?
+    assert_equal 2, cruiser.health
+  end
+
+  def test_ship_sinks_after_being_hit_3_times
+    # skip
+    cruiser = Ship.new("Cruiser", 3)
+    cruiser.hit
+    cruiser.hit
+    cruiser.hit
+  assert_equal true, cruiser.sunk?
+  end
 end
