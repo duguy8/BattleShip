8cbf039622a070cfe49b63121bacd0cece0a142d
Trevor Suter <Trevorsuter@icloud.com>
2020-12-15 14:41:22 -0700

    added runner file with initial setup

---
+++ lib/runner.rb
@@ -0,0 +1,14 @@
+require './ship'
+require './cell'
+require './board'
+require './computer'
+require './player'
+require './turn'
+require './game'
+
+cruiser = Ship.new("Cruiser", 3)
+submarine = Ship.new("Submarine", 2)
+player = Player.new
+computer = Computer.new
+turn = Turn.new(player, computer)
+game = Game.new
